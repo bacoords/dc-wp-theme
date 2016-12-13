19fedb821337a6c982fe3a2bb2c770ea35515d4c
Brian Coords <bacoords@gmail.com>
2016-12-12 21:33:24 -0800

    updates

--- sidebar.php
+++ sidebar.php
@@ -14,7 +14,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
   <aside id="bio" class="widget widget_bio">
     <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/family.jpg" alt="Family">
-    <h2 class="widget_bio__name">Hi, I'm Dayna!</h2>
+    <h2 class="widget_bio__name">Welcome</h2>
     <p class="widget_bio__desc">We all desire to be understood. To simply share our hearts and be heard. This blog fulfills that need. Here I share my heart, the good and the bad, as I navigate through being a wife, a mother, sister, daughter, and friend.</p>
     
 <!--    <h4 class="widget_bio__social-header text-xs-center">Connect with Me</h4>-->
