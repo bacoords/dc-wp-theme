921fadaeb7c6fddaa6307ea3d8f7ebc965a55937
Brian Coords <bacoords@gmail.com>
2017-02-17 22:08:07 -0800

    remove title

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -8,11 +8,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header">
+<!--	<header class="entry-header">-->
 
-		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+		<?php // the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 
-	</header><!-- .entry-header -->
+<!--	</header> .entry-header -->
 
      <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?> 
     
